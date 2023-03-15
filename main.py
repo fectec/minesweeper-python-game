d11883dcb85112d67b11c5815e91452fe3e65bda
Juan Manuel Félix Campos <fectec151@gmail.com>
2023-03-14 23:07:20 -0600

    Added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+
+# Instantiate a window instance
+
+root = Tk()
+
+# Changing window size and title
+
+root.geometry('1440x720')
+root.title("Minesweeper game")
+
+# Prohibit window resizing
+
+root.resizable(False, False)
+
+# Main loop
+
+root.mainloop()
+
