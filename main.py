c5f2337c0e4d277528877e733c8fd5a8510387e4
Juan Manuel Félix Campos <fectec151@gmail.com>
2023-03-14 23:22:35 -0600

    Created top and left frames

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
 from tkinter import *
 
-# Instantiate a window instance
+# Instantiating a window instance
 
 root = Tk()
+
+# Changing window background color
+
+root.configure(bg = "black")
 
 # Changing window size and title
 
 root.geometry('1440x720')
 root.title("Minesweeper game")
 
-# Prohibit window resizing
+# Prohibiting window resizing
 
 root.resizable(False, False)
 
-# Main loop
+# Creating top frame
+
+top_frame = Frame(
+    root,
+    bg = 'red', # Change later to black
+    width = 1440,
+    height = 180
+)
+
+top_frame.place(x = 0, y = 0)
+
+# Creating left frame
+
+left_frame = Frame(
+    root,
+    bg = 'blue', # Change later to black
+    width = 360,
+    height = 540
+)
+
+left_frame.place(x = 0, y = 180)
+
+# Running the window
 
 root.mainloop()
-
