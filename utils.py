f50d04c54d58612f4042927e03e8d55d2b7b062a
Juan Manuel Félix Campos <fectec151@gmail.com>
2023-03-23 17:38:00 -0600

    Added documentation comments on window_dimensions_percentage funtion

--- utils.py
+++ utils.py
@@ -3,6 +3,11 @@
 import settings
 
 def window_dimensions_percentage(dimension, percentage):
+
+    """
+    @param dimension: True for Width, False for Heigth
+    @param percentage: Percentage of the dimension value
+    """
     
     if dimension == True:
 
